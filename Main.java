e955cf600e35f7e0819d2f14b820a8f1549ba976
ellag7121 <ellag7121@wrdsb.ca>
2020-09-15 13:32:25 +0000

    Finished A2Q1!

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * get a number from the user, tell the user if the number is odd or even
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // import a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //initialize variables
+    int integer; //the integer that is provided by the user
+    int remainder; //the remainder of the integer when divided by 2
+
+    //initialize constants
+    final int EVEN = 0; //the remainder for an even number divided by 2
+
+    //prompt user input
+    System.out.println("Please enter an integer:"); //ask user for input
+    integer = input.nextInt(); //store the integer in the ineger variable
+
+    //get the remainder of the integer divided by 2
+    remainder = integer%2;
+
+    //check if the remainder is even
+    if (remainder == EVEN){
+      //tell the user that their number is even
+      System.out.println(integer + " is an even number");
+    }else{ //do this if the remainder is odd
+      //tell the user that their number is odd
+      System.out.println(integer + " is an odd number");
+    }
   }
 }
